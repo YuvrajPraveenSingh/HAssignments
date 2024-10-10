61ed170fc426d447685c0761c9e57946ff01e9a6
YuvrajPraveenSingh <yuv00191@outlook.com>
2024-10-10 22:37:37 +0530

    Refactor anagram function to improve performance and readability
    - Added a check to compare the lengths of the input strings before proceeding with the anagram check
    - Sorted the characters of both strings to ensure accurate comparison
    - Updated the return statement to return true if the strings are anagrams, and false otherwise

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const str1Arr = str1.split('').sort();
+  const str2Arr = str2.split('').sort();
+
+  for (let i = 0; i < str1Arr.length; i++) {
+    if (str1Arr[i] !== str2Arr[i]) {
+      return false;
+    }
+  }
+
+  return true;
+
 
 }
 
